12d7e2fce5c958d8b1bef238800bf6ff72bb2c0c
Victor <victorportela1010@gmail.com>
2025-04-30 16:05:13 -0300

    30/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+
+// Pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className='container'>
-      <h1>Mini Blog</h1>
+    <div className="App">
+      <BrowserRouter>
+      <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
