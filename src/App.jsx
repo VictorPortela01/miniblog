4b22c259d9cfcec2257a8d870e7bad00690ef446
Victor <victorportela1010@gmail.com>
2025-05-07 08:15:15 -0300

    06.05

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+//hooks
+import { useState,useEffect } from "react";
+import { useAuthentication } from "./components/hooks/useAuthentication";
+
+
 import "./App.css";
 
 // context
@@ -11,11 +18,31 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  }, [auth])
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <Navbar />
           <div className="container">
@@ -24,6 +51,8 @@ const App = () => {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />}/>
+              <Route path="/dashboard" element={<Dashboard />}/>
             </Routes>
           </div>
           <Footer />
